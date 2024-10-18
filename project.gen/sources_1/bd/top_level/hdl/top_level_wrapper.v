12eb63de287b975fdedfccea9c99b139565dafd4
DouglasWWolf <douglaswolf@yahoo.com>
2024-10-18 15:13:32 -0700

    No commit message

--- project.gen/sources_1/bd/top_level/hdl/top_level_wrapper.v
+++ project.gen/sources_1/bd/top_level/hdl/top_level_wrapper.v
@@ -1,7 +1,7 @@
 //Copyright 1986-2021 Xilinx, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2021.1 (lin64) Build 3247384 Thu Jun 10 19:36:07 MDT 2021
-//Date        : Tue Oct 15 13:29:53 2024
+//Date        : Tue Oct 15 19:05:33 2024
 //Host        : simtool-5 running 64-bit Ubuntu 20.04.6 LTS
 //Command     : generate_target top_level_wrapper.bd
 //Design      : top_level_wrapper
@@ -28,6 +28,22 @@ module top_level_wrapper
     m0_ddr4_odt,
     m0_ddr4_par,
     m0_ddr4_reset_n,
+    m1_ddr4_act_n,
+    m1_ddr4_adr,
+    m1_ddr4_ba,
+    m1_ddr4_bg,
+    m1_ddr4_ck_c,
+    m1_ddr4_ck_t,
+    m1_ddr4_cke,
+    m1_ddr4_clk_clk_n,
+    m1_ddr4_clk_clk_p,
+    m1_ddr4_cs_n,
+    m1_ddr4_dq,
+    m1_ddr4_dqs_c,
+    m1_ddr4_dqs_t,
+    m1_ddr4_odt,
+    m1_ddr4_par,
+    m1_ddr4_reset_n,
     pcie_mgt_rxn,
     pcie_mgt_rxp,
     pcie_mgt_txn,
@@ -79,6 +95,22 @@ module top_level_wrapper
   output [0:0]m0_ddr4_odt;
   output m0_ddr4_par;
   output m0_ddr4_reset_n;
+  output m1_ddr4_act_n;
+  output [16:0]m1_ddr4_adr;
+  output [1:0]m1_ddr4_ba;
+  output [1:0]m1_ddr4_bg;
+  output [0:0]m1_ddr4_ck_c;
+  output [0:0]m1_ddr4_ck_t;
+  output [0:0]m1_ddr4_cke;
+  input m1_ddr4_clk_clk_n;
+  input m1_ddr4_clk_clk_p;
+  output [0:0]m1_ddr4_cs_n;
+  inout [71:0]m1_ddr4_dq;
+  inout [17:0]m1_ddr4_dqs_c;
+  inout [17:0]m1_ddr4_dqs_t;
+  output [0:0]m1_ddr4_odt;
+  output m1_ddr4_par;
+  output m1_ddr4_reset_n;
   input [15:0]pcie_mgt_rxn;
   input [15:0]pcie_mgt_rxp;
   output [15:0]pcie_mgt_txn;
@@ -131,6 +163,22 @@ module top_level_wrapper
   wire [0:0]m0_ddr4_odt;
   wire m0_ddr4_par;
   wire m0_ddr4_reset_n;
+  wire m1_ddr4_act_n;
+  wire [16:0]m1_ddr4_adr;
+  wire [1:0]m1_ddr4_ba;
+  wire [1:0]m1_ddr4_bg;
+  wire [0:0]m1_ddr4_ck_c;
+  wire [0:0]m1_ddr4_ck_t;
+  wire [0:0]m1_ddr4_cke;
+  wire m1_ddr4_clk_clk_n;
+  wire m1_ddr4_clk_clk_p;
+  wire [0:0]m1_ddr4_cs_n;
+  wire [71:0]m1_ddr4_dq;
+  wire [17:0]m1_ddr4_dqs_c;
+  wire [17:0]m1_ddr4_dqs_t;
+  wire [0:0]m1_ddr4_odt;
+  wire m1_ddr4_par;
+  wire m1_ddr4_reset_n;
   wire [15:0]pcie_mgt_rxn;
   wire [15:0]pcie_mgt_rxp;
   wire [15:0]pcie_mgt_txn;
@@ -184,6 +232,22 @@ module top_level_wrapper
         .m0_ddr4_odt(m0_ddr4_odt),
         .m0_ddr4_par(m0_ddr4_par),
         .m0_ddr4_reset_n(m0_ddr4_reset_n),
+        .m1_ddr4_act_n(m1_ddr4_act_n),
+        .m1_ddr4_adr(m1_ddr4_adr),
+        .m1_ddr4_ba(m1_ddr4_ba),
+        .m1_ddr4_bg(m1_ddr4_bg),
+        .m1_ddr4_ck_c(m1_ddr4_ck_c),
+        .m1_ddr4_ck_t(m1_ddr4_ck_t),
+        .m1_ddr4_cke(m1_ddr4_cke),
+        .m1_ddr4_clk_clk_n(m1_ddr4_clk_clk_n),
+        .m1_ddr4_clk_clk_p(m1_ddr4_clk_clk_p),
+        .m1_ddr4_cs_n(m1_ddr4_cs_n),
+        .m1_ddr4_dq(m1_ddr4_dq),
+        .m1_ddr4_dqs_c(m1_ddr4_dqs_c),
+        .m1_ddr4_dqs_t(m1_ddr4_dqs_t),
+        .m1_ddr4_odt(m1_ddr4_odt),
+        .m1_ddr4_par(m1_ddr4_par),
+        .m1_ddr4_reset_n(m1_ddr4_reset_n),
         .pcie_mgt_rxn(pcie_mgt_rxn),
         .pcie_mgt_rxp(pcie_mgt_rxp),
         .pcie_mgt_txn(pcie_mgt_txn),
